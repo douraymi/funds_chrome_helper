759233a838f1df00c15ed297a21a326691fd4cf3
douraymi <douraymi@gmail.com>
2015-04-22 18:31:47 +0800

    .

--- code/js/modules/chrome_cab.js
+++ code/js/modules/chrome_cab.js
@@ -24,6 +24,33 @@ module.exports = {
 		.done(function(data){
 			callback(data);			
 		})
-  }
+  },
+  storage : function(){
+		this.set = function(items, callback){
+			chrome.storage.sync.set(items, callback);
+		};
+		this.get = function(keys, callback){
+			chrome.storage.sync.get(keys, callback);
+		};
+		this.onKeyChange = function(listenerObj){
+			var _obj = vendor.OBJnw(listenerObj);
+			_.each(_obj, function(func, key){
+				chrome.onChanged.addListener(func);		
+			});
+		}
+		this.rmKeyChange = function(listenerObj){
+			var _obj = vendor.OBJnw(listenerObj);
+			_.each(_obj, function(func, key){
+				chrome.onChanged.addListener(func);		
+			});
+		}
+		this.bind = function(domObj, key){
+
+		}
+		this.unBind = function(){
+
+		}
+		return this;
+	}()
 
 }
--- code/js/modules/chrome_storage.js
+++ code/js/modules/chrome_storage.js
@@ -1,26 +1,29 @@
 'use strict'
 var $ = require('jquery');
+var _ = require('underscore');
+var vendor = require('./girafeee_vendor');
 
-function storageDef(){
-	var df = $.Deferred();
-	this.onKeyChange = function(key){
-		chrome.onChanged.addListener(function(changes){
-			// callback(changes[key]);
-			$.resolve(changes[key]);
-		});
-	}
-	return df;
-}
 
 function storage (argument) {
 
-	this.set = function(key, value){
-		chrome.storage.sync.set();
+	this.set = function(items, callback){
+		chrome.storage.sync.set(items, callback);
 	};
-	this.onKeyChange = function(key){
-		
+	this.get = function(keys, callback){
+		chrome.storage.sync.get(keys, callback);
+	};
+	this.onKeyChange = function(listenerObj){
+		var _obj = vendor.OBJnw(listenerObj);
+		_.each(_obj, function(func, key){
+			chrome.onChanged.addListener(func);		
+		});
+	}
+	this.rmKeyChange = function(listenerObj){
+		var _obj = vendor.OBJnw(listenerObj);
+		_.each(_obj, function(func, key){
+			chrome.onChanged.addListener(func);		
+		});
 	}
 	return this;
 }
-
-module.exports = new storage();
+module.exports = storage();
